a236fa7ccfa1b44b6f14733e4504e1a5cb41d593
Amal Zakirov <116391159+azaiv@users.noreply.github.com>
2024-08-30 11:02:22 +0300

    Prepare project

---
+++ Main App/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        let vc = WelcomeViewController()
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.tintColor = .tintColor
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
+    }
+}
+
